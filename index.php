2691ace8a6649f4b3780ac471cb577dc49f1cc76
Miguel Ángel Rodríguez Sojo <mrodsoj898a@ieslacampina.es>
2025-04-24 20:45:45 +0200

    Insertar la barra de búsqueda

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,16 +8,26 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
+
 <body>
     <header>
-       <div class="container py-2">
+        <div class="container py-2">
             <a href="index.php">CineRate</a>
-            
-       </div>
+            <!-- Barra de búsqueda -->
+            <div class="col-md-5">
+                <div class="input-group">
+                    <input type="text" class="form-control search-box" placeholder="Buscar películas, actores...">
+                    <button class="btn search-btn" type="button">
+                        <i class="bi bi-search"></i>
+                    </button>
+                </div>
+            </div>
+        </div>
     </header>
 
 
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
+
 </html>
