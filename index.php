21e04be9f7c8b3092da7afe04ba7e8ee63ccf8b0
Miguel Ángel Rodríguez Sojo <mrodsoj898a@ieslacampina.es>
2025-04-25 21:36:28 +0200

    Colorear el menu usando css y estructurarlo

--- index.php
+++ index.php
@@ -5,11 +5,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CineRate</title>
-    <link rel="stylesheet" href="index.css">
+    <link rel="icon" href="./Imagenes/Logo_fondo_blanco.png" type="image/x-icon">
+    <link rel="stylesheet" href="inicio.css" type="text/css">
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-     <!-- Bootstrap Icons -->
-     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.0/font/bootstrap-icons.css">
+    <!-- Bootstrap Icons -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.0/font/bootstrap-icons.css">
 </head>
 
 <body>
@@ -18,14 +19,14 @@
             <div class="row align-items-center">
                 <!-- Barra de búsqueda -->
                 <div class="col-md-3">
-                    <a href="index.php"> <img src="./Imagenes/Logo.png" alt="Logo" class="img-fluid" style="max-height: 60px;"></a>
+                    <a href="index.php"> <img src="./Imagenes/Logo_negro.png" alt="Logo" class="img-fluid" style="max-height: 60px;"></a>
 
                 </div>
                 <!-- Barra de búsqueda -->
                 <div class="col-md-5">
                     <div class="input-group">
                         <input type="text" class="form-control search-box" placeholder="Buscar películas, actores...">
-                        <button class="btn search-btn" type="button">
+                        <button class="btn search-btn" type="button"> <!--Icono de buscar-->
                             <i class="bi bi-search"></i>
                         </button>
                     </div>
@@ -37,41 +38,41 @@
                 </div>
             </div>
         </div>
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark py-1">
+        <nav class="navbar navbar-expand-md navbar-dark bg-dark py-2"> <!--Categoria se combierte en icono desplazable-->
             <div class="container">
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCine" aria-controls="navbarCine" aria-expanded="false" aria-label="Toggle navigation">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#categoriaCine" aria-controls="navbarCine" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse" id="navbarCine">
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <div class="collapse navbar-collapse" id="categoriaCine">
+                    <ul class="navbar-nav me-auto">
                         <li class="nav-item">
-                            <a class="nav-link nav-link-cine active" href="#"><i class="bi bi-house-door"></i> Inicio</a>
+                            <a class="nav-link categoria-cine active" href="index.php"><i class="bi bi-house-door"></i> Inicio</a>
                         </li>
                         <li class="nav-item dropdown">
-                            <a class="nav-link nav-link-cine dropdown-toggle" href="#" id="navbarDropdownGeneros" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            <a class="nav-link categoria-cine dropdown-toggle" href="#" id="icono-menu" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                 <i class="bi bi-tags"></i> Géneros
                             </a>
-                            <ul class="dropdown-menu dropdown-menu-cine" aria-labelledby="navbarDropdownGeneros">
-                                <li><a class="dropdown-item dropdown-item-cine" href="#">Acción</a></li>
-                                <li><a class="dropdown-item dropdown-item-cine" href="#">Drama</a></li>
-                                <li><a class="dropdown-item dropdown-item-cine" href="#">Comedia</a></li>
-                                <li><a class="dropdown-item dropdown-item-cine" href="#">Ciencia Ficción</a></li>
-                                <li><a class="dropdown-item dropdown-item-cine" href="#">Terror</a></li>
-                                <li><a class="dropdown-item dropdown-item-cine" href="#">Romance</a></li>
+                            <ul class="dropdown-menu dropdown-menu-genero" aria-labelledby="icono-menu">
+                                <li><a class="dropdown-item categoria-cine" href="accion.php">Acción</a></li>
+                                <li><a class="dropdown-item categoria-cine" href="drama.php">Drama</a></li>
+                                <li><a class="dropdown-item categoria-cine" href="comedia.php">Comedia</a></li>
+                                <li><a class="dropdown-item categoria-cine" href="cienciaficcion.php">Ciencia Ficción</a></li>
+                                <li><a class="dropdown-item categoria-cine" href="terror.php">Terror</a></li>
+                                <li><a class="dropdown-item categoria-cine" href="romance.php">Romance</a></li>
                                 <!-- Estos se cargarían dinámicamente desde tu tabla GENERO -->
                             </ul>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link nav-link-cine" href="#"><i class="bi bi-people"></i> Actores</a>
+                            <a class="nav-link categoria-cine" href="actores.php"><i class="bi bi-people"></i> Actores</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link nav-link-cine" href="#"><i class="bi bi-camera-reels"></i> Directores</a>
+                            <a class="nav-link categoria-cine" href="directores.php"><i class="bi bi-camera-reels"></i> Directores</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link nav-link-cine" href="#"><i class="bi bi-trophy"></i> Premios</a>
+                            <a class="nav-link categoria-cine" href="premios.php"><i class="bi bi-trophy"></i> Premios</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link nav-link-cine" href="#"><i class="bi bi-chat-left-text"></i> Comentarios</a>
+                            <a class="nav-link categoria-cine" href="comentarios.php"><i class="bi bi-chat-left-text"></i> Comentarios</a>
                         </li>
                     </ul>
                 </div>
